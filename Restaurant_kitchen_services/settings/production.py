842defc9aa03928f5547f327f8036a928d05182a
Alexey <alopar632@gmail.com>
2025-07-09 13:20:51 +0300

    Fix ALLOWED_HOSTS for production

--- Restaurant_kitchen_services/settings/production.py
+++ Restaurant_kitchen_services/settings/production.py
@@ -3,8 +3,9 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
-
+ALLOWED_HOSTS = [
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME', "restaurant-kitchen-services-g13m.onrender.com")
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
@@ -17,6 +18,7 @@ DATABASES = {
         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
         "HOST": os.environ["POSTGRES_HOST"],
         "PORT": int(os.environ["POSTGRES_DB_PORT"]),
-
+        "OPTIONS": {
+           'sslmode': 'require',
     }
 }
